a1cb5b6261dcc21f1aa1138d2d3f5f6d7dfdff29
katetorrejon <katetorrejon@gmail.com>
2026-05-27 22:15:41 +0800

    Update BarangayApp.java

--- BarangayApp.java
+++ BarangayApp.java
@@ -7,6 +7,7 @@ public class BarangayApp {
         Scanner input = new Scanner(System.in);
         DatabaseManager DB = new DatabaseManager();
         LoginManager LM = new LoginManager(DB);
+        ReportManager RM = new ReportManager(DB);
         
         boolean runAdminMenu = false;
         boolean runUserMenu = false;
@@ -47,7 +48,7 @@ public class BarangayApp {
                         String residentID = input.nextLine();
 
                         if (DB.residentExists(residentID)) {
-                            if (DB.residentAccountExists(residentID)) {
+                            if (DB.userAccountExists(residentID)) {
                                 System.out.println("Resident already has an account.");
                             } else {
                                 String accountType;
@@ -204,12 +205,96 @@ public class BarangayApp {
                         break;
 
                     case "3":
+                        ArrayList<Document> documents = DB.getDocuments("documents");
+                        System.out.println("\n--- Document Requests ---");
+                        if (!documents.isEmpty()) {
+                            for (Document document : documents) {
+                                System.out.println("ID: " + document.getID() + " | " + document.getResidentID() + " | " + document.getDocType() + " | Status: " + document.getStatus());
+                            }
+                            System.out.print("Enter Document ID: ");
+                            int docID = input.nextInt();
+                            input.nextLine();
+
+                            int count = 0;
+                            for (Document document : documents) {
+                                if (document.getID() == docID) {
+                                    System.out.println("\nID: " + document.getID() + " | " + document.getResidentID() + " | " + document.getDocType() + " | Status: " + document.getStatus());
+                                    count++;
+                                    break;
+                                }
+                            }
+                            if (count == 0) {
+                                System.out.println("Document with ID '" + docID + "' does not exist.");
+                            } else {
+                                System.out.println("Actions:");
+                                System.out.println("1] Approve");
+                                System.out.println("2] Mark as Ready for Pickup");
+                                System.out.println("3] Reject");
+                                System.out.print("Enter your choice [1-3]: ");
+                                String statusChoice = input.nextLine();
+
+                                switch (statusChoice) {
+                                    case "1":
+                                        if (DB.changeStatus("Approved", docID)) {
+                                            System.out.println("Request approved.");
+                                        } else {
+                                            System.out.println("Failed to approve request.");
+                                        }
+                                        break;
+
+                                    case "2":
+                                        for (Document document : documents) {
+                                            if (document.getID() == docID) {
+                                                if (!document.getStatus().equalsIgnoreCase("Rejected")) {
+                                                    if (DB.deleteRequest(docID)) {
+                                                        if (DB.addIssuance(document.getResidentID(), document.getDocType(), "Ready")) {
+                                                            System.out.println("Request ready for pickup.");
+                                                        } else {
+                                                            System.out.println("Failed to update request.");
+                                                        }
+                                                    } else {
+                                                        System.out.println("Failed to update request.");
+                                                    }
+                                                } else {
+                                                    System.out.println("Cannot mark a rejected request as ready for pickup.");
+                                                }
+                                                break;
+                                            }
+                                        }
+                                        break;
+
+                                    case "3":
+                                        if (DB.changeStatus("Rejected", docID)) {
+                                            System.out.println("Request rejected.");
+                                        } else {
+                                            System.out.println("Failed to reject request.");
+                                        }
+                                        break;
+
+                                    default:
+                                        System.out.println("Invalid input. Please enter a number from 1 to 3.");
+                                }
+                            }
+
+                        } else {
+                            System.out.println("No requests found.");
+                        }
                         break;
 
                     case "4":
+                        ArrayList<Document> issuances = DB.getDocuments("issuances");
+                        System.out.println("\n--- Issuances ---");
+                        if (!issuances.isEmpty()) {
+                            for (Document issuance : issuances) {
+                                System.out.println("ID: " + issuance.getID() + " | " + issuance.getResidentID() + " | " + issuance.getDocType() + " | Status: " + issuance.getStatus());
+                            }
+                        } else {
+                            System.out.println("No issuances found.");
+                        }
                         break;
 
                     case "5":
+                        RM.viewBlotterReports(input);
                         break;
 
                     case "6":
@@ -229,6 +314,10 @@ public class BarangayApp {
             }
 
             while (runUserMenu) {
+                String residentID = DB.getResidentID(creds.getUsername(), creds.getPassword());
+                Resident resident = DB.getResident(residentID);
+
+                System.out.println("\nHello, " + resident.getFirstName() + "!");
                 System.out.println("\n--- MENU ---");
                 System.out.println("1] Request a document");
                 System.out.println("2] File a report");
@@ -240,12 +329,208 @@ public class BarangayApp {
 
                 switch (choice) {
                     case "1":
+                        boolean runDocMenu = true;
+                        while (runDocMenu) {
+                            System.out.println("\nDocuments:");
+                            System.out.println("1] Barangay Clearance");
+                            System.out.println("2] Barangay ID");
+                            System.out.println("3] Certificate of Residency");
+                            System.out.println("4] Certificate of Indigency");
+                            System.out.println("5] Barangay Business Permit");
+                            System.out.println("6] Barangay Building Clearance");
+                            System.out.println("7] Back");
+                            System.out.print("Enter your choice [1-7]: ");
+                            String docChoice = input.nextLine();
+
+                            switch (docChoice) {
+                                case "1":
+                                    Document barangayClearance = new BarangayCLR();
+                                    barangayClearance.displayRequirements();
+
+                                    while (true) {
+                                        System.out.print("\nSubmit? [Y/N]: ");
+                                        String submitChoice = input.nextLine();
+                                        if (submitChoice.equalsIgnoreCase("Y")) {
+                                            if (DB.addDocument(residentID, barangayClearance.getDocType(), "Pending")) {
+                                                System.out.println("Request successful.");
+                                            } else {
+                                                System.out.println("Failed to request document.");
+                                            }
+                                            break;
+                                        } else if (submitChoice.equalsIgnoreCase("N")) {
+                                            break;
+
+                                        } else {
+                                            System.out.println("Invalid input. Please enter Y or N.");
+                                        }
+                                    }
+                                    break;
+
+                                case "2":
+                                    Document barangayID = new BarangayID();
+                                    barangayID.displayRequirements();
+
+                                    while (true) {
+                                        System.out.print("\nSubmit? [Y/N]: ");
+                                        String submitChoice = input.nextLine();
+                                        if (submitChoice.equalsIgnoreCase("Y")) {
+                                            if (DB.addDocument(residentID, barangayID.getDocType(), "Pending")) {
+                                                System.out.println("Request successful.");
+                                            } else {
+                                                System.out.println("Failed to request document.");
+                                            }
+                                            break;
+                                        } else if (submitChoice.equalsIgnoreCase("N")) {
+                                            break;
+
+                                        } else {
+                                            System.out.println("Invalid input. Please enter Y or N.");
+                                        }
+                                    }
+                                    break;
+
+                                case "3":
+                                    Document cor = new BarangayCOR();
+                                    cor.displayRequirements(resident);
+
+                                    while (true) {
+                                        System.out.print("\nSubmit? [Y/N]: ");
+                                        String submitChoice = input.nextLine();
+                                        if (submitChoice.equalsIgnoreCase("Y")) {
+                                            if (DB.addDocument(residentID, cor.getDocType(), "Pending")) {
+                                                System.out.println("Request successful.");
+                                            } else {
+                                                System.out.println("Failed to request document.");
+                                            }
+                                            break;
+                                        } else if (submitChoice.equalsIgnoreCase("N")) {
+                                            break;
+
+                                        } else {
+                                            System.out.println("Invalid input. Please enter Y or N.");
+                                        }
+                                    }
+                                    break;
+
+                                case "4":
+                                    Document coi = new BarangayCOI();
+                                    coi.displayRequirements();
+
+                                    while (true) {
+                                        System.out.print("\nSubmit? [Y/N]: ");
+                                        String submitChoice = input.nextLine();
+                                        if (submitChoice.equalsIgnoreCase("Y")) {
+                                            if (DB.addDocument(residentID, coi.getDocType(), "Pending")) {
+                                                System.out.println("Request successful.");
+                                            } else {
+                                                System.out.println("Failed to request document.");
+                                            }
+                                            break;
+                                        } else if (submitChoice.equalsIgnoreCase("N")) {
+                                            break;
+
+                                        } else {
+                                            System.out.println("Invalid input. Please enter Y or N.");
+                                        }
+                                    }
+                                    break;
+
+                                case "5":
+                                    Document businessPermit = new BarangayBNP();
+                                    businessPermit.displayRequirements();
+
+                                    while (true) {
+                                        System.out.print("\nSubmit? [Y/N]: ");
+                                        String submitChoice = input.nextLine();
+                                        if (submitChoice.equalsIgnoreCase("Y")) {
+                                            if (DB.addDocument(residentID, businessPermit.getDocType(), "Pending")) {
+                                                System.out.println("Request successful.");
+                                            } else {
+                                                System.out.println("Failed to request document.");
+                                            }
+                                            break;
+                                        } else if (submitChoice.equalsIgnoreCase("N")) {
+                                            break;
+
+                                        } else {
+                                            System.out.println("Invalid input. Please enter Y or N.");
+                                        }
+                                    }
+                                    break;
+
+                                case "6":
+                                    Document buildingClearance = new BarangayBDC();
+                                    buildingClearance.displayRequirements();
+
+                                    while (true) {
+                                        System.out.print("\nSubmit? [Y/N]: ");
+                                        String submitChoice = input.nextLine();
+                                        if (submitChoice.equalsIgnoreCase("Y")) {
+                                            if (DB.addDocument(residentID, buildingClearance.getDocType(), "Pending")) {
+                                                System.out.println("Request successful.");
+                                            } else {
+                                                System.out.println("Failed to request document.");
+                                            }
+                                            break;
+                                        } else if (submitChoice.equalsIgnoreCase("N")) {
+                                            break;
+
+                                        } else {
+                                            System.out.println("Invalid input. Please enter Y or N.");
+                                        }
+                                    }
+                                    break;
+
+                                case "7":
+                                    runDocMenu = false;
+                                    break;
+
+                                default:
+                                    System.out.println("Invalid input. Please enter a number from 1 to 7.");
+                            }
+                        }
                         break;
 
                     case "2":
+                        RM.fileReport(input, residentID);
                         break;
 
                     case "3":
+                        System.out.println("\n--- Document Requests ---");
+
+                        ArrayList<Document> readyDocuments = DB.getDocuments("issuances", residentID, "Ready");
+                        ArrayList<Document> approvedDocuments = DB.getDocuments("documents", residentID, "Approved");
+                        ArrayList<Document> pendingDocuments = DB.getDocuments("documents", residentID, "Pending");
+                        ArrayList<Document> rejectedDocuments = DB.getDocuments("documents", residentID, "Rejected");
+
+                        if (readyDocuments.isEmpty() && approvedDocuments.isEmpty() && pendingDocuments.isEmpty() && rejectedDocuments.isEmpty()) {
+                            System.out.println("You haven't made any document requests yet.");
+                        } else {
+                            if (!readyDocuments.isEmpty()) {
+                                System.out.println("\n[ READY FOR PICKUP ]");
+                                for (Document document : readyDocuments) {
+                                    System.out.println("ID: " + document.getID() + " | " + document.getDocType() + " | Status: " + document.getStatus());
+                                }
+                            }
+                            if (!approvedDocuments.isEmpty()) {
+                                System.out.println("\n[ APPROVED ]");
+                                for (Document document : approvedDocuments) {
+                                    System.out.println("ID: " + document.getID() + " | " + document.getDocType() + " | Status: " + document.getStatus());
+                                }
+                            }
+                            if (!pendingDocuments.isEmpty()) {
+                                System.out.println("\n[ PENDING ]");
+                                for (Document document : pendingDocuments) {
+                                    System.out.println("ID: " + document.getID() + " | " + document.getDocType() + " | Status: " + document.getStatus());
+                                }
+                            }
+                            if (!rejectedDocuments.isEmpty()) {
+                                System.out.println("\n[ REJECTED ]");
+                                for (Document document : rejectedDocuments) {
+                                    System.out.println("ID: " + document.getID() + " | " + document.getDocType() + " | Status: " + document.getStatus());
+                                }
+                            }
+                        }
                         break;
 
                     case "4":
