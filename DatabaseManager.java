f12d92996125e658d63d3980d7649e613628d339
katetorrejon <katetorrejon@gmail.com>
2026-05-27 22:16:30 +0800

    Update DatabaseManager.java

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -43,6 +43,63 @@ public class DatabaseManager {
         }
     }
 
+    public String getResidentID(String username, String password) {
+        String query = "SELECT * FROM credentials WHERE username = ? AND password = ?";
+        String residentID = "";
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    residentID = resultSet.getString(4);
+                }
+                return residentID;
+            }
+
+        } catch (SQLException e) {
+            return residentID;
+        }
+    }
+
+    public Resident getResident(String residentID) {
+        String query = "SELECT * FROM residents WHERE residentID = ?";
+        Resident resident = null;
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            
+            preparedStatement.setString(1, residentID);
+            
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    resident = new Resident();
+
+                    resident.setResidentID(resultSet.getString(2));
+                    resident.setLastName(resultSet.getString(3));
+                    resident.setFirstName(resultSet.getString(4));
+                    resident.setMiddleName(resultSet.getString(5));
+                    resident.setSex(resultSet.getString(6));
+                    resident.setDOB(resultSet.getDate(7).toLocalDate());
+                    resident.setPOB(resultSet.getString(8));
+                    resident.setHouseholdID(resultSet.getInt(9));
+                    resident.setCivilStatus(resultSet.getString(10));
+                    resident.setCitizenship(resultSet.getString(11));
+                    resident.setOccupation(resultSet.getString(12));
+                    resident.setResidencyStart(resultSet.getDate(13).toLocalDate());
+                    resident.setAddress(getAddress(resultSet.getInt(9)));
+                }
+                return resident;
+            }
+
+        } catch (SQLException e) {
+            return resident;
+        }
+    }
+
     public ArrayList<Resident> getResidents() {
         ArrayList<Resident> residentList = new ArrayList<>();
         String query = "SELECT * FROM residents";
@@ -66,6 +123,8 @@ public class DatabaseManager {
                     resident.setCitizenship(resultSet.getString(11));
                     resident.setOccupation(resultSet.getString(12));
                     resident.setResidencyStart(resultSet.getDate(13).toLocalDate());
+                    resident.setAddress(getAddress(resultSet.getInt(9)));
+                    
 
                     residentList.add(resident);
                 }
@@ -76,6 +135,33 @@ public class DatabaseManager {
         }
 
         return residentList;
+    }
+
+    public String getAddress(int householdID) {
+        String query = "SELECT * FROM households WHERE id = ?";
+        String address = "";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+
+                preparedStatement.setInt(1, householdID);
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        int houseNum = resultSet.getInt(2);
+                        String street = resultSet.getString(3);
+                        String purok = resultSet.getString(4);
+                        String barangay = resultSet.getString(5);
+                        String city = resultSet.getString(6);
+                        String province = resultSet.getString(7);
+
+                        address = "" + houseNum + " " + street + ", Purok " + purok + ", Barangay " + barangay + ", " + city + ", " + province;
+                    }
+                    return address;
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return address;
+        }
     }
 
     public boolean residentExists(String residentID) {
@@ -94,13 +180,14 @@ public class DatabaseManager {
         }
     }
 
-    public boolean residentAccountExists(String residentID) {
-        String query = "SELECT * FROM credentials WHERE residentID = ?";
+    public boolean userAccountExists(String residentID) {
+        String query = "SELECT * FROM credentials WHERE residentID = ? AND role = ?";
 
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
              PreparedStatement preparedStatement = conn.prepareStatement(query)) {
 
                 preparedStatement.setString(1, residentID);
+                preparedStatement.setString(2, "user");
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     return resultSet.next();
                 }
@@ -157,4 +244,318 @@ public class DatabaseManager {
         }
     }
 
+    public boolean addDocument(String residentID, String documentType, String status) {
+        String query = "INSERT INTO documents (residentID, document, status) VALUES (?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+
+                preparedStatement.setString(1, residentID);
+                preparedStatement.setString(2, documentType);
+                preparedStatement.setString(3, status);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+                return rowsAffected > 0;
+             
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean addIssuance(String residentID, String documentType, String status) {
+        String query = "INSERT INTO issuances (residentID, document, status) VALUES (?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+
+                preparedStatement.setString(1, residentID);
+                preparedStatement.setString(2, documentType);
+                preparedStatement.setString(3, status);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+                return rowsAffected > 0;
+             
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public ArrayList<Document> getDocuments(String table) {
+        ArrayList<Document> docList = new ArrayList<>();
+        String query = "SELECT * FROM " + table;
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = conn.createStatement()) {
+
+            try (ResultSet resultSet = statement.executeQuery(query)) {
+                while (resultSet.next()) {
+                    String docType = resultSet.getString(3);
+                    Document document = null;
+
+                    if (docType.equalsIgnoreCase("Barangay Clearance")) {
+                        document = new BarangayCLR(); 
+                    } else if (docType.equalsIgnoreCase("Barangay ID")) {
+                        document = new BarangayID(); 
+                    } else if (docType.equalsIgnoreCase("Certificate of Residency")) {
+                        document = new BarangayCOR(); 
+                    } else if (docType.equalsIgnoreCase("Certificate of Indigency")) {
+                        document = new BarangayCOI(); 
+                    } else if (docType.equalsIgnoreCase("Barangay Business Permit")) {
+                        document = new BarangayBNP(); 
+                    } else if (docType.equalsIgnoreCase("Barangay Building Clearance")) {
+                        document = new BarangayBDC(); 
+                    }
+
+                    document.setID(resultSet.getInt(1));
+                    document.setResidentID(resultSet.getString(2));
+                    document.setStatus(resultSet.getString(4));
+
+                    if (document != null) {
+                        docList.add(document);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return docList;
+    }
+
+    public ArrayList<Document> getDocuments(String table, String residentID, String status) {
+        ArrayList<Document> docList = new ArrayList<>();
+        String query = "SELECT * FROM " + table + " WHERE residentID = ? AND status = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+
+            preparedStatement.setString(1, residentID);
+            preparedStatement.setString(2, status);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    String docType = resultSet.getString(3);
+                    Document document = null;
+
+                    if (docType.equalsIgnoreCase("Barangay Clearance")) {
+                        document = new BarangayCLR(); 
+                    } else if (docType.equalsIgnoreCase("Barangay ID")) {
+                        document = new BarangayID(); 
+                    } else if (docType.equalsIgnoreCase("Certificate of Residency")) {
+                        document = new BarangayCOR(); 
+                    } else if (docType.equalsIgnoreCase("Certificate of Indigency")) {
+                        document = new BarangayCOI(); 
+                    } else if (docType.equalsIgnoreCase("Barangay Business Permit")) {
+                        document = new BarangayBNP(); 
+                    } else if (docType.equalsIgnoreCase("Barangay Building Clearance")) {
+                        document = new BarangayBDC(); 
+                    }
+
+                    document.setID(resultSet.getInt(1));
+                    document.setResidentID(resultSet.getString(2));
+                    document.setStatus(resultSet.getString(4));
+
+                    if (document != null) {
+                        docList.add(document);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return docList;
+    }
+
+    public boolean changeStatus(String status, int id) {
+        String query = "UPDATE documents SET status = ? WHERE id = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+
+            preparedStatement.setString(1, status);
+            preparedStatement.setInt(2, id);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteRequest(int id) {
+        String query = "DELETE FROM documents WHERE id = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, id);
+
+            int rowsDeleted = preparedStatement.executeUpdate();
+            return rowsDeleted > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public int fileBlotterReport(Report report) {
+        String query = "INSERT INTO report (reporterResidentID, nReport, iDate, iTime, iMeridiem, iHouseNo, iStreet, iPurok, iBarangay, iCity, description, mediation, status, dtFiled) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement ps = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+
+            ps.setString(1, report.getReporterResidentID());
+            ps.setString(2, report.getNatureofReport());
+            ps.setString(3, report.getIncidentDate());
+            ps.setString(4, report.getIncidentTime());
+            ps.setString(5, report.getIncidentMeridiem());
+            ps.setString(6, report.getIncidentHouseNo());
+            ps.setString(7, report.getIncidentStreet());
+            ps.setString(8, report.getIncidentPurok());
+            ps.setString(9, report.getIncidentBarangay());
+            ps.setString(10, report.getIncidentCity());
+            ps.setString(11, report.getDesc());
+            ps.setBoolean(12, report.getMed());
+            ps.setString(13, report.getStatus());
+
+            ps.executeUpdate();
+
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                return generatedKeys.getInt(1); // return blotterID
+            }
+            return -1; //failed
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public boolean addRespondent(Respondent respondent){
+        String query = "INSERT INTO respondent (blotterID, respondent, residentID) VALUES (?,?,?)";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setInt(1, respondent.getBlotterID());
+            ps.setString(2, respondent.getRespondent());
+            if (respondent.getResidentID() != null) {
+                ps.setString(3, respondent.getResidentID());
+            } else {
+                ps.setNull(3, java.sql.Types.VARCHAR);
+            }
+            
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public ArrayList<Report> getBlotterReports() {
+        ArrayList<Report> bReportList = new ArrayList<>();
+        String query = "SELECT * FROM report";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = conn.createStatement()) {
+
+            try (ResultSet rs = statement.executeQuery(query)) {
+                while (rs.next()) {
+                    Report report = new Report(rs.getInt(1), rs.getString(15));
+
+                    report.setReporterResidentID(rs.getString(2));
+                    report.setNatureofReport(rs.getString(3));
+                    report.setIncidentDate(rs.getString(4));
+                    report.setIncidentTime(rs.getString(5));
+                    report.setIncidentMeridiem(rs.getString(6));
+                    report.setIncidentHouseNo(rs.getString(7));
+                    report.setIncidentStreet(rs.getString(8));
+                    report.setIncidentPurok(rs.getString(9));
+                    report.setIncidentBarangay(rs.getString(10));
+                    report.setIncidentCity(rs.getString(11));
+                    report.setDesc(rs.getString(12));
+                    report.setMed(rs.getBoolean(13));
+                    report.setStatus(rs.getString(14));
+
+                    bReportList.add(report);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return bReportList;
+    }
+
+    public ArrayList<Respondent> getRespondents(int blotterID){
+        ArrayList<Respondent> respondentList = new ArrayList<>();
+        String query = "SELECT * FROM respondent WHERE blotterID = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                PreparedStatement ps = conn.prepareStatement(query)) {
+            
+            ps.setInt(1, blotterID);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Respondent respondent = new Respondent(rs.getInt(1));
+
+                    respondent.setRespondent(rs.getString(3));
+                    respondent.setResidentID(rs.getString(4));
+                    respondentList.add(respondent);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return respondentList;
+    }
+
+    public boolean updateBlotterStatus(int blotterID, String status){
+        String query = "UPDATE report SET status = ? WHERE blotterID = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement ps = conn.prepareStatement(query)) {
+            
+            ps.setString(1, status);
+            ps.setInt(2, blotterID);
+
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public ArrayList<Resident> findResident(String lastName) {
+        ArrayList<Resident> residentList = new ArrayList<>();
+        String query = "SELECT * FROM residents WHERE lname = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, lastName);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Resident resident = new Resident();
+                    resident.setResidentID(rs.getString(2));
+                    resident.setLastName(rs.getString(3));
+                    resident.setFirstName(rs.getString(4));
+                    resident.setMiddleName(rs.getString(5));
+                    residentList.add(resident);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return residentList;
+    }
 }
